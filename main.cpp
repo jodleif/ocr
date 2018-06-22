d599b86eaf9a5dced20006c2fe4d46c5f7c6862a
Jo Øivind Gjernes <jodleif@gmail.com>
2018-06-22 10:45:29 +0200

    Use std::filesystem to check if file exists

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <experimental/filesystem>
 #include "ocr_lib.h"
 
 namespace {
-void print_results(std::optional<std::string> str) {
-    if(str) {
+void print_results(std::optional<std::string> str)
+{
+    if (str) {
         std::cout << *str << '\n';
     } else {
         std::cout << "Could not load image\n";
@@ -11,13 +13,21 @@ void print_results(std::optional<std::string> str) {
 }
 }
 
-int main()
+int main(int argc, const char **argv)
 {
+    if (argc < 2) return 1;
+    {
+        std::error_code ec;
+        auto path = std::experimental::filesystem::path(argv[1]);
+        auto exists = std::experimental::filesystem::exists(path, ec);
+        if (!exists && !ec) {
+            std::cout << "File: " << path << " does not exist\n";
+            return 1;
+        }
+    }
     ocr::ImageOcr img;
 
-    auto str = img.read_image("test.png");
+    auto str = img.read_image(argv[1]);
     print_results(str);
-    auto str2 = img.read_image("test2.png");
-    print_results(str2);
     return 0;
 }
